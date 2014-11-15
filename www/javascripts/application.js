1577f0d8733eb010e9e6f5779e01f93fe251e009
Joe Roberts <joeroberts@RAD-MAC-1582.local>
2014-11-15 10:44:09 -0500

    setup initial commit

---
+++ www/javascripts/application.js
@@ -0,0 +1,5 @@
+// Display the native navigation bar with the title "Hello World!"
+//steroids.view.navigationBar.show("Meal Team");
+
+// Set the WebView background color to white (effective on iOS only)
+steroids.view.setBackgroundColor("#6674DD");
